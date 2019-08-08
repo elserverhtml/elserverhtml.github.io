0eb85ab0748e3bdb2d1f35e72e553bbaf50c6319
Elecshen <51473599+Elecshen@users.noreply.github.com>
2019-08-08 15:18:21 +0700

    Доработка статистики и добавление номеров вопросов

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function btnNextPage(numpage) {
-    var page = document.getElementById("Ar" + (numpage + 1));
+    var page = document.getElementById("Ar" + (numpage + 1)),
+        progres = document.getElementsByClassName("currentprogres")[0];
+    
     page.style.transform = "translate(-100%)";
-    var progres = document.getElementsByClassName("currentprogres")[0];
     progres.style.transform = "translate(" + (100 * numpage) + "%)";
 }
 
 function btnPrevPage(numpage) {
-    var page = document.getElementById("Ar" + numpage);
+    var page = document.getElementById("Ar" + numpage),
+        progres = document.getElementsByClassName("currentprogres")[0];
+    
     page.style.transform = "translate(0)";
-    var progres = document.getElementsByClassName("currentprogres")[0];
     progres.style.transform = "translate(" + (100 * (numpage - 2)) + "%)";
 }
 
@@ -26,79 +28,108 @@ var RightAnswer = [
 ];
 
 function isRight(nQuestion, nAnswer) {
+    console.log(nQuestion + " " + nAnswer);
+    
     var elems = document.querySelectorAll(".answer");
     if (RightAnswer[nQuestion][0] === nAnswer) {
         elems[nQuestion * 4 + nAnswer - 1].style.background = "#36ff45";
         RightAnswer[nQuestion][1] = nAnswer;
+        
+        console.log(RightAnswer[nQuestion][1] + " " + nAnswer);
     } else {
         elems[nQuestion * 4 + nAnswer - 1].style.background = "#ff3636";
         RightAnswer[nQuestion][1] = nAnswer;
         elems[nQuestion * 4 + RightAnswer[nQuestion][0] - 1].style.background = "#36ff45";
     }
-    for(let i = 0; i < 4; i++){
-        elems[nQuestion * 4 + i].onclick = function(){};
+    for (let i = 0; i < 4; i++) {
+        elems[nQuestion * 4 + i].onclick = function () {};
     }
-    setTimeout(function(){
+    setTimeout(function () {
         elems = document.querySelectorAll(".questionBody");
-        elems[nQuestion+1].style.transform = "translateY(-100%)";
-    }, 1000)
+        elems[nQuestion + 1].style.transform = "translateY(-100%)";
+    }, 1);
+    
+    if (nQuestion === 19) {
+        getStat();
+    }
+}
+
+function btnMore() {
+    var Height = "",
+        charsH = this.previousElementSibling.style.maxHeight.split('');
+    
+    for (let i = 0; i < charsH.length - 2; i++) {
+        Height += charsH[i];
+    }
+    
+    Height = Number.parseInt(Height);
+    
+    if (Height >= this.previousElementSibling.children.length * 506) {
+        Height = 506 * 2;
+    } else if (Height < this.previousElementSibling.children.length * 506 && Height > 0) {
+        Height += 506 * 2;
+    } else {
+        Height = 506 * 4;
+    }
+    
+    if (Height >= this.previousElementSibling.children.length * 506) {
+        this.firstChild.innerHTML = "&uarr; Свернуть &uarr;";
+    } else {
+        this.firstChild.innerHTML = "&darr; Развернуть &darr;";
+    }
+    
+    Height += "px";
+    this.previousElementSibling.style.maxHeight = Height;
+}
+
+function getStat() {
+    var statlist = document.getElementsByClassName("statYourAnswer"),
+        answers = document.getElementsByClassName("answer"),
+        rightAns = document.getElementsByClassName("statRightAnswer");
+    for (let i = 0; i < statlist.length; i++) {
+        statlist[i].innerHTML = answers[i*4 + RightAnswer[i][1] - 1].firstChild.innerHTML;
+        
+        console.log(i + " " + (i*4 + RightAnswer[i][1] - 1) + " " + RightAnswer[i][1]);
+        
+        if (RightAnswer[i][0] != RightAnswer[i][1]) {
+            statlist[i].parentElement.parentElement.style.background = "#ff3636";
+            
+            statlist[i].parentElement.parentElement.parentElement.onmouseover = function () {
+                this.firstElementChild.style.background = "#ff6f6f";
+            }
+            
+            statlist[i].parentElement.parentElement.parentElement.onmouseout = function () {
+                this.firstElementChild.style.background = "#ff3636";
+            }
+            
+            rightAns[i].innerHTML = answers[i*4 + RightAnswer[i][0] - 1].firstChild.innerHTML;
+            
+            statlist[i].parentElement.parentElement.parentElement.addEventListener("click", openCorrectAnswer);
+        }
+    }
+}
+
+function openCorrectAnswer() {
+    if (this.style.height != "80px") {
+        this.style.height = "80px";
+    } else {
+        this.style.height = "40px";
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll(".answer");
     for (let i = 0; i < elems.length; i++) {
         elems[i].onclick = function () {
+            console.log(Math.floor(i/4) + " " + i%4 + 1);
             isRight(Math.floor(i/4), i%4 + 1);
-        }
-        
-        if(elems.length - 5 >= i){
-            elems[i].onclick = function () {
-                isRight(Math.floor(i/4), i%4 + 1);
-                getStat();
-            }
-        }
+        };
     }
     
-    for (let i = 0; i < document.getElementsByClassName("morebtn").length; i++)
-    document.getElementsByClassName("morebtn")[i].addEventListener("click", btnMore)
+    for (i = 0; i < document.getElementsByClassName("morebtn").length; ++i) {
+        document.getElementsByClassName("morebtn")[i].addEventListener("click", btnMore);
+    }
 });
-
-function btnMore() {
-    var Height = "";
-    var charsH = this.previousElementSibling.style.maxHeight.split('');
-    for(let i = 0; i < charsH.length - 2; i++)
-        Height += charsH[i];
-    Height = Number.parseInt(Height);
-    
-    if(Height >= this.previousElementSibling.children.length*506) {
-       Height = 506*2;
-    } else if (Height < this.previousElementSibling.children.length*506 && Height > 0){
-        Height += 506*2;
-    } else {
-        Height = 506*4;
-    }
-    
-    if(Height >= this.previousElementSibling.children.length*506)
-        this.firstChild.innerHTML = "&uarr; Свернуть &uarr;";
-    else this.firstChild.innerHTML = "&darr; Развернуть &darr;";
-    
-    Height += "px";
-    this.previousElementSibling.style.maxHeight = Height;
-}
-
-function getStat(){
-    var statlist = document.getElementsByClassName("statYourAnswer");
-    var answers = document.getElementsByClassName("answer");
-    for(let i = 0; i < statlist.length; i++){
-        statlist[i].innerHTML = answers[i * 4 + RightAnswer[i][1] - 1].firstChild.innerHTML;
-        
-        if(RightAnswer[i][0] == RightAnswer[i][1]){
-            statlist[i].parentElement.parentElement.style.background = "#36ff45";
-        }
-    }
-}
-
-
 
 
 
